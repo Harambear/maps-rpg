0e4b0718dfe8130fbf8048e59e30f13487ad9e64
Ki Jung Kim <kimkijung@icloud.com>
2024-10-12 14:36:59 -1000

    modify boilerplate code

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import MapComponent from './components/map/MapComponent'
+import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // check if there is an existing profile
+  // MapComponent should accept profile
+  // profiles should contain location data
+  // lack of profile as prop can trigger new instantiation with current location and / or default location / s
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <section className="app">
+      <MapComponent />
+    </section>
   )
 }
 
