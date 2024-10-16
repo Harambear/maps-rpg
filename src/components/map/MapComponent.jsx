78cb885d882617dbfabf18c36c354460f643f0c8
Ki Jung Kim <kimkijung@icloud.com>
2024-10-16 07:30:59 -1000

    Add encounter objectives to map

--- src/components/map/MapComponent.jsx
+++ src/components/map/MapComponent.jsx
@@ -1,30 +1,54 @@
 import React, { useState, useEffect } from 'react';
-import { APIProvider, Map } from '@vis.gl/react-google-maps';
+import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
+import EncounterComponent from '../encounter/EncounterComponent';
 import MarkerComponent from '../marker/MarkerComponent';
 import PlaceComponent from '../place/PlaceComponent';
-
+import balloons from '../../assets/icons/balloons.png';
 import './MapComponent.scss';
 
 export default function MapComponent({ player, setProfile }) {
-  const [position, setPosition] = useState(player.getLocation());
-  const [coordinates, setCoordinates] = useState(null);
   const [characterState, setCharacterState] = useState('idle');
-  const [placeId, setPlaceId] = useState();
+  const [encounterObjectives, setEncounterObjectives] = useState([]);
+  const [coordinates, setCoordinates] = useState(null);
+  const [openPanorama, setOpenPanorama] = useState(null);
+  const [panorama, setPanorama] = useState(null);
+  const [placeId, setPlaceId] = useState(null);
+  const [position, setPosition] = useState(player.getLocation());
 
   useEffect(() => {
     player.setLocation(position);
     localStorage.setItem('profile', JSON.stringify(player));
   }, [position])
 
-  function clickHandler(event) {
-    event.stop();
-
+  function mapClickHandler(event) {
     const placeId = event.detail.placeId;
 
     if (placeId) {
       event.stop();
       setPlaceId(placeId);
     }
+  }
+
+  function objectiveClickHandler(event) {
+    const filteredObjectives = encounterObjectives.filter((objective) => {
+      return (
+        objective.lat !== this.position.lat() ||
+        objective.lng !== this.position.lng()
+      )
+    });
+
+    // if no objectives are left
+    if (!filteredObjectives.length) {
+      // turn off panorama
+      panorama.setVisible(false);
+
+      // show text?
+    }
+
+    // give reward
+    player.adjustCoins(10);
+    console.log(player);
+    return setEncounterObjectives(filteredObjectives);
   }
 
   const googleMapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
@@ -41,9 +65,34 @@ export default function MapComponent({ player, setProfile }) {
           disableDoubleClickZoom={true}
           scrollwheel={false}
           keyboardShortcuts={false}
-          onClick={clickHandler}
+          onClick={mapClickHandler}
           backgroundColor={'black'}
         >
+          {
+            encounterObjectives.length ?
+              encounterObjectives.map((objective, index) => {
+                return (
+                  <Marker
+                    key={index}
+                    className='encounter__objective'
+                    position={objective}
+                    icon={balloons}
+                    onClick={objectiveClickHandler}
+                  />
+                )
+              }) :
+              null
+          }
+          {
+            (!!openPanorama) ?
+              <EncounterComponent
+                position={position}
+                characterState={characterState}
+                setOpenPanorama={setOpenPanorama}
+                setEncounterObjectives={setEncounterObjectives}
+                setPanorama={setPanorama} /> :
+              null
+          }
           {
             (!!placeId) ?
               <PlaceComponent
@@ -61,7 +110,8 @@ export default function MapComponent({ player, setProfile }) {
               setCharacterState={setCharacterState}
               coordinates={coordinates}
               setPosition={setPosition}
-              setPlaceId={setPlaceId} />
+              setPlaceId={setPlaceId}
+              setOpenPanorama={setOpenPanorama} />
           }
         </Map>
       </APIProvider>
